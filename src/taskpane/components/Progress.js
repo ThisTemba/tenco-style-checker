7bdecafd326b4f049930761c0a63816fb279e95d
ThisTemba <tembamateke@gmail.com>
2022-05-13 13:29:26 -0400

    Conditionally render logo&title in Progress

--- src/taskpane/components/Progress.js
+++ src/taskpane/components/Progress.js
@@ -8,8 +8,8 @@ export default class Progress extends React.Component {
 
     return (
       <section className="ms-welcome__progress ms-u-fadeIn500">
-        <img width="90" height="90" src={logo} alt={title} title={title} />
-        <h1 className="ms-fontSize-su ms-fontWeight-light ms-fontColor-neutralPrimary">{title}</h1>
+        {logo && <img width="90" height="90" src={logo} alt={title} title={title} />}
+        {title && <h1 className="ms-fontSize-su ms-fontWeight-light ms-fontColor-neutralPrimary">{title}</h1>}
         <Spinner size={SpinnerSize.large} label={message} />
       </section>
     );
